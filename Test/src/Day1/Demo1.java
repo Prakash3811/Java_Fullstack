96c3330789c2017ecf051291d721c5746cab103f
pk845 <pk845@DESKTOP-UQDLQ0M>
2025-07-27 10:32:21 +0530

    first Commit

---
+++ Test/src/Day1/Demo1.java
@@ -0,0 +1,14 @@
+package Day1;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+    }
+}
---
+++ Test/src/Day1/Demo2.java
@@ -0,0 +1,52 @@
+package Day1;
+
+class Demo2 {
+    public static void main(String args[]) {
+//        int a = 10;
+//        int b = 20;
+//
+//        System.out.println("++a = " + (++a));
+//        System.out.println("--b = " + (--b));
+    	
+//        System.out.println("a == b = " + (a == b));
+//        System.out.println("a != b = " + (a != b));
+//        System.out.println("a > b  = " + (a > b));
+//        System.out.println("a < b  = " + (a < b));
+//        System.out.println("b >= a = " + (b >= a));
+//        System.out.println("b <= a = " + (b <= a));
+//        boolean a = true;
+//        boolean b = false;
+//
+//        System.out.println("a && b = " + (a && b));
+//        System.out.println("a || b = " + (a || b));
+//        System.out.println("!(a && b) = " + !(a && b));
+    	
+//    	int x = 7;
+//        int y = 9;
+//        int z = x & y;
+//        System.out.println("z = " + z);
+    	
+//    	int x = 5;
+//        int y = 9;
+//        int z = x ^ y;
+//        System.out.println("z = " + z);
+    	int i_val = 10, j_val = 20;
+        boolean chk;
+        chk = i_val < j_val;
+        System.out.println("chk value: " + chk);
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
